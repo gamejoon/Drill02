74561a8f861d321ae3800a4303407fe155c53929
gamejoon <gamejoon0325@naver.com>
2023-09-18 15:21:11 +0900

    Drill02 제출 (2021180038 조영준)

--- character_grass.py
+++ character_grass.py
@@ -8,17 +8,58 @@ os.chdir("D:\\2DGP\\Drill02")
 grass = load_image('grass.png')
 character = load_image("character.png")
 
+import math
 
-x = 0
-while (x < 800):
-    # Game Rendering
+x = 400
+y = 90
+
+move_shape = ["square", "circle"]
+now_shape = move_shape[1]
+
+def draw_picture(x, y):
     clear_canvas_now()
     grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    
-    # Game Logic
-    x = x + 2
-    
+    character.draw_now(x, y)
     delay(0.01)
+
+def move_square():
+    global x, y
+    while(x < 800):
+        x += 2
+        draw_picture(x, y)
+
+    while(y < 600):
+        y += 2
+        draw_picture(x, y)
+
+    while (x > 0):
+        x -= 2
+        draw_picture(x, y)
+
+    while (y > 90):
+        y -= 2
+        draw_picture(x, y)
+
+    while (x < 400):
+        x += 2
+        draw_picture(x, y)
+
+def move_circle():
+    global x, y
+    theta = 270
+    while(theta > -90):
+        theta -= 1
+        x = math.cos(theta * math.pi / 180) * 250 + 400
+        y = (math.sin(theta * math.pi / 180) + 1) * 250 + 90
+        draw_picture(x, y)
+
+while (1):
+    
+    if now_shape == "square":
+        move_square()
+        now_shape = move_shape[1]
+    elif now_shape == "circle":
+        move_circle()
+        now_shape = move_shape[0]
 
 close_canvas()
